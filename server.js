58ccebeee59364a7d4b7ff62184bb14936549396
Fadi Haddad <fadi.k.haddad@gmail.com>
2023-03-07 01:31:18 +0300

    Add adder funcionality

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/add", (req, res) => {
+    const {x, y} = req.query;
+    const sum = parseInt(x) + parseInt(y);
+    res.send(200, sum);
+});
+
 app.get("/", (req, res) => {
     res.send(200, "Hello World");
 });
