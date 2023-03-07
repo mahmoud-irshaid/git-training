34f1f83942ad1c15f90fa0aa328087a8b331cef9
Fadi Haddad <fadi.k.haddad@gmail.com>
2023-03-07 20:44:35 +0300

    Add operations module

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const { add } = require("./operations");
 const { parseOperands } = require("./utils");
 
 const app = express();
 
 app.get("/add", (req, res) => {
     const {x, y} = parseOperands(req);
-    const sum = parseInt(x) + parseInt(y);
+    const sum = add(x, y);
     res.send(200, sum);
 });
 
