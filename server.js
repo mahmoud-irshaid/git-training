fc6dc559d57d80ad9640cf1e5dc45b9dc5fb45db
mahmoud-irshaid <mirshaid10@gmail.com>
2023-03-19 15:49:01 +0300

    Refactor the addition operation

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { add } = require("./operations");
+const { parseOperands } = require("./utils");
 
 const app = express();
 
 app.get("/add", (req, res) => {
-    const {x, y} = req.query;
-    const sum = parseInt(x) + parseInt(y);
+    const {x, y} = parseOperands(req);
+    const sum = add(x, y);
     res.send(200, sum);
 });
 
